5a869689e93bac328c6645c291d85285abcf5c90
Christopher Wells <chrisw@wellswebtech.co.za>
2025-03-01 22:26:18 +0200

    Update index.vue

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
+// const getProjects = async () => {
+//   //...
+//   const { data, error } = await supabase.from('projects').select()
+
+//   if (error) console.log(error)
+
+//   console.log('Projects : ', data)
+// }
+
+let projects: any
+;(async () => {
+  // ...
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.log(error)
+
+  projects = data
+
+  //console.log('Projects : ', data)
+  console.log('projects: ', projects)
+})()
+
+//await getProjects()
+</script>
 
 <template>
-    <div>
-        <h1>Projects Page</h1>
-        <RouterLink to="/">Go to home</RouterLink>
-    </div>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+    {{ projects }}
+  </div>
 </template>
