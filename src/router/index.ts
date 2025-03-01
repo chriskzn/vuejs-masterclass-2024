cf49479bddd0339c915f817bfb1d5fa6eeaac531
Christopher Wells <chrisw@wellswebtech.co.za>
2025-03-01 18:55:30 +0200

    Files for chapter 2 video 14
    Files for chapter 2 video 14
    1. npm install -D unplugin-vue-router
    2. edit vite.config.ts file, add the following: import VueRouter from 'unplugin-vue-router/vite'
    3. Add VueRouter(), under the plugins section of the vite.config.ts file
    4. modify the file index.ts, change the import to this: import { createRouter, createWebHistory } from 'vue-router/auto'
    5. Remove all the routes currently in the index.ts, also remove the imports so it is only shows step 4
    6. Rename the folder 'views' to 'pages'
    7. Add import { routes } from 'vue-router/auto-routes' to the index.ts file
    8. Add routes to the const router = createRouter({
      history: createWebHistory(import.meta.env.BASE_URL), section

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+//import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+//import HomeView from '@/views/HomeView.vue'
 //import ProjectsView from '@/views/ProjectsView.vue'
-import { h } from 'vue'
+//import { h } from 'vue'
+
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue')
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue')
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'NotFound',
-      component: h('p', {style: 'color: red;'}, '404 Not Found')
-    },
-  ],
+  routes,
+  // routes: [
+  //   {
+  //     path: '/',
+  //     name: 'home',
+  //     component: HomeView
+  //   },
+  //   {
+  //     path: '/projects',
+  //     name: 'projects',
+  //     component: () => import('@/views/ProjectsView.vue')
+  //   },
+  //   {
+  //     path: '/projects/:id',
+  //     name: 'single-project',
+  //     component: () => import('@/views/SingleProjectView.vue')
+  //   },
+  //   {
+  //     path: '/:catchAll(.*)*',
+  //     name: 'NotFound',
+  //     component: h('p', {style: 'color: red;'}, '404 Not Found')
+  //   },
+  // ],
 })
 
 export default router
