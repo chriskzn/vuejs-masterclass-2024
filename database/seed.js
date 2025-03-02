9375e6bad4822c46d5b925a208840762820e8636
Christopher Wells <chrisw@wellswebtech.co.za>
2025-03-02 14:48:02 +0200

    Chapter 3 Video 32
    Chapter 3 Video 32 - Create a New Page for Tasks with Database Migration, Seed and Types

--- database/seed.js
+++ database/seed.js
@@ -8,9 +8,21 @@ const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE
 
 //console.log(supabase)
 
+const logErrorAndExit = (tableName, error) => {
+  console.error(
+    `An error occurred in table '${tableName}' with code ${error.code}: ${error.message}`,
+  )
+  process.exit(1)
+}
+
+const logStep = (stepMessage) => {
+  console.log(stepMessage)
+}
+
 const seedProjects = async (numEntries) => {
   // ...
   //const name = faker.lorem.words(3)
+  logStep('Seeding projects...')
   const projects = []
   for (let i = 0; i < numEntries; i++) {
     const name = faker.lorem.words(3)
@@ -27,7 +39,46 @@ const seedProjects = async (numEntries) => {
   //   status: faker.helpers.arrayElement(['in-progress', 'completed']),
   //   collaborators: faker.helpers.arrayElements([1, 2, 3]),
   // })
-  await supabase.from('projects').insert(projects)
+  const { data, error } = await supabase.from('projects').insert(projects).select('id')
+
+  if (error) return logErrorAndExit('Projects', error)
+
+  logStep('Projects seeded successfully.')
+
+  return data
 }
 
-await seedProjects(10)
+const seedTasks = async (numEntries, projectsIds) => {
+  logStep('Seeding tasks...')
+  const tasks = []
+
+  for (let i = 0; i < numEntries; i++) {
+    tasks.push({
+      name: faker.lorem.words(3),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      description: faker.lorem.paragraph(),
+      due_date: faker.date.future(),
+      project_id: faker.helpers.arrayElement(projectsIds),
+      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+    })
+  }
+
+  const { data, error } = await supabase.from('tasks').insert(tasks).select('id')
+
+  if (error) return logErrorAndExit('Tasks', error)
+
+  logStep('Tasks seeded successfully.')
+
+  return data
+}
+
+const seedDatabase = async (numEntriesPerTable) => {
+  const projectsIds = (await seedProjects(numEntriesPerTable)).map((project) => project.id)
+  await seedTasks(numEntriesPerTable, projectsIds)
+}
+
+const numEntriesPerTable = 10
+
+seedDatabase(numEntriesPerTable)
+
+//await seedProjects(10)
